612697a8ead3a226b00a612f663217255d07140e
Le Khac Man <mle29@csc.com>
2017-12-22 14:55:22 +0700

    login form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(event);
+        fetch("http://localhost:9999/auth").then(res => {
+            console.log(res.json());
+        });
+    }
   render() {
     return (
       <div className="App">
@@ -10,9 +17,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div style={{marginBottom: "50px"}}></div>
+        <main>
+            <form onSubmit={this.submitHandler}>
+                <div className="form-group">
+                    Email:<input type="email" className="form-controller" required={true}/>
+                </div>
+                <div className="form-group">
+                    Password: <input type="password" className="form-controller" required={true}/>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </main>
       </div>
     );
   }
